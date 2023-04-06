994faa34373463c108dfb42bcf6557098f246e7d
septiandjamari <septiannico50@gmail.com>
2023-04-06 23:06:51 +0700

    commit1 6 april 2023

--- lib/ui/8.manajemen_pengguna/1.manajemen_pengguna.dart
+++ lib/ui/8.manajemen_pengguna/1.manajemen_pengguna.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:program_inventaris/global_database/5.manajemen_akun.dart';
 import 'package:program_inventaris/ui/8.manajemen_pengguna/2.dialog_tambah_edit_pengguna.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -12,48 +13,21 @@ class ManajemenPengguna extends StatefulWidget {
   State<ManajemenPengguna> createState() => _ManajemenPenggunaState();
 }
 
-List<Map<String, dynamic>> listAkun = [
-  {
-    "id_user": "esqPpMy4",
-    "username": "admin_riswanda",
-    "real_name": "Riswanda Imawan",
-    "password": "katasandi",
-    "role": "Pegawai Invetaris",
-    "privileges": "admin",
-    "path_photo_profile": "",
-  },
-  {
-    "id_user": "q2ydPfbfw3",
-    "username": "ahli_sujadi",
-    "real_name": "Ahli Sujadi",
-    "password": "masahlisujadi123",
-    "role": "Kepala Sekolah",
-    "privileges": "viewer",
-    "path_photo_profile": "",
-  },
-  {
-    "id_user": "ght3DfrIop",
-    "username": "super_admin",
-    "real_name": "Super Admin",
-    "password": "super_admin",
-    "role": "Super Admin",
-    "privileges": "super_admin",
-    "path_photo_profile": "",
-  },
-  {
-    "id_user": "Iopghfrt3D",
-    "username": "super_admin1",
-    "real_name": "Super Admin",
-    "password": "super_admin",
-    "role": "Super Admin",
-    "privileges": "super_admin",
-    "path_photo_profile": "",
-  },
-];
-
 class _ManajemenPenggunaState extends State<ManajemenPengguna> {
   bool hidePasswordVisibility = true;
   Map<String, dynamic> userInfo = {};
+
+  List<Map<String, dynamic>> listAkun = [];
+
+  void loadListAkun() async {
+    var initialData = await streamListAkunProvider.dataListAkunValue(viewOrEditListAkun: "list");
+    setState(() {
+      listAkun = initialData;
+    });
+    if (kDebugMode) {
+      print(listAkun);
+    }
+  }
 
   void loadUserInfo() async {
     SharedPreferences sp = await SharedPreferences.getInstance();
@@ -69,6 +43,7 @@ class _ManajemenPenggunaState extends State<ManajemenPengguna> {
   void initState() {
     super.initState();
     loadUserInfo();
+    loadListAkun();
   }
 
   void addEditDialog({required String addOrEdit, Map<String, dynamic>? userData}) {
@@ -142,105 +117,110 @@ class _ManajemenPenggunaState extends State<ManajemenPengguna> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 1,
-        backgroundColor: Colors.white,
-        title: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text(
-                  "Data tersimpan : ",
-                  style: TextStyle(fontSize: 12, color: Colors.black),
-                ),
-                Text(
-                  "${listAkun.length}",
-                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 12, color: Colors.black),
-                ),
-              ],
+    return StreamBuilder<List<Map<String, dynamic>>>(
+        initialData: listAkun,
+        stream: streamListAkunProvider.getStream,
+        builder: (context, snapshot) {
+          return Scaffold(
+            appBar: AppBar(
+              elevation: 1,
+              backgroundColor: Colors.white,
+              title: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        "Data tersimpan : ",
+                        style: TextStyle(fontSize: 12, color: Colors.black),
+                      ),
+                      Text(
+                        "${listAkun.length - 1}",
+                        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 12, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.blue.shade700),
+                    onPressed: () {
+                      addEditDialog(addOrEdit: "add");
+                    },
+                    child: const Text("TAMBAH PENGGUNA"),
+                  )
+                ],
+              ),
             ),
-            TextButton(
-              style: TextButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.blue.shade700),
-              onPressed: () {
-                addEditDialog(addOrEdit: "add");
-              },
-              child: const Text("TAMBAH PENGGUNA"),
-            )
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: listAkun.map(
-            (e) {
-              int index = listAkun.indexOf(e) + 1;
-              return e["username"] != userInfo["username"]
-                  ? ExpansionTile(
-                      leading: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "$index.",
-                            style: const TextStyle(color: Colors.black54),
-                          ),
-                        ],
-                      ),
-                      title: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            "${e["real_name"]} - ",
-                            style: const TextStyle(color: Colors.black),
-                          ),
-                          Text(
-                            e["username"],
-                            style: const TextStyle(color: Colors.black54, fontSize: 14),
-                          ),
-                        ],
-                      ),
-                      subtitle: Text(
-                        "${e["role"]} / ${e["privileges"]}",
-                        style: const TextStyle(color: Colors.black54),
-                      ),
-                      children: [
-                        const Divider(),
-                        Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
+            body: SingleChildScrollView(
+              child: Column(
+                children: listAkun.map(
+                  (e) {
+                    int index = listAkun.indexOf(e) + 1;
+                    return e["username"] != userInfo["username"]
+                        ? ExpansionTile(
+                            leading: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "$index.",
+                                  style: const TextStyle(color: Colors.black54),
+                                ),
+                              ],
+                            ),
+                            title: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  "${e["real_name"]} - ",
+                                  style: const TextStyle(color: Colors.black),
+                                ),
+                                Text(
+                                  e["username"],
+                                  style: const TextStyle(color: Colors.black54, fontSize: 14),
+                                ),
+                              ],
+                            ),
+                            subtitle: Text(
+                              "${e["role"]} / ${e["privileges"]}",
+                              style: const TextStyle(color: Colors.black54),
+                            ),
                             children: [
-                              ElevatedButton.icon(
-                                style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow.shade800),
-                                onPressed: () {
-                                  addEditDialog(addOrEdit: "edit", userData: e);
-                                },
-                                icon: const Icon(Icons.edit_rounded),
-                                label: const Text("EDIT DATA"),
-                              ),
-                              const SizedBox(width: 16),
-                              ElevatedButton.icon(
-                                onPressed: () {
-                                  resetPassword(namaPengguna: e["real_name"]);
-                                },
-                                icon: const Icon(Icons.restart_alt_rounded),
-                                label: const Text("RESET PASSWORD"),
+                              const Divider(),
+                              Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    ElevatedButton.icon(
+                                      style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow.shade800),
+                                      onPressed: () {
+                                        addEditDialog(addOrEdit: "edit", userData: e);
+                                      },
+                                      icon: const Icon(Icons.edit_rounded),
+                                      label: const Text("EDIT DATA"),
+                                    ),
+                                    const SizedBox(width: 16),
+                                    ElevatedButton.icon(
+                                      onPressed: () {
+                                        resetPassword(namaPengguna: e["real_name"]);
+                                      },
+                                      icon: const Icon(Icons.restart_alt_rounded),
+                                      label: const Text("RESET PASSWORD"),
+                                    )
+                                  ],
+                                ),
                               )
                             ],
-                          ),
-                        )
-                      ],
-                    )
-                  : const SizedBox();
-            },
-          ).toList(),
-        ),
-      ),
-    );
+                          )
+                        : const SizedBox();
+                  },
+                ).toList(),
+              ),
+            ),
+          );
+        });
   }
 }
 
